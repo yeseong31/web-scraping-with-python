f77821cd08c128db53afe57a128951dbbd52fbbe
Yeseong31 <yeseong31@naver.com>
2021-09-10 18:48:55 +0900

    0910

--- webscraping_basic/10_bs4_coupane_pages.py
+++ webscraping_basic/10_bs4_coupane_pages.py
@@ -16,13 +16,11 @@ for i in range(1, 6):
         # 광고 제품 제외
         ad_badge = item.find("span", attrs={"class": "ad-badge-text"})
         if ad_badge:
-            # print("---<광고 상품 제외>---")
             continue
 
         name = item.find("div", attrs={"class": "name"}).get_text()
         # Apple 제품은 제외
         if 'Apple' in name:
-            # print("---<Apple 상품 제외>---")
             continue
 
         price = item.find("strong", attrs={"class": "price-value"}).get_text()
@@ -32,8 +30,6 @@ for i in range(1, 6):
         if rate:
             rate = rate.get_text()
         else:
-            rate = '평점 없음'
-            # print('---<평점 없는 상품 제외>---')
             continue
 
         rate_cnt = item.find("span", attrs={"class": "rating-total-count"})
@@ -41,14 +37,11 @@ for i in range(1, 6):
             rate_cnt = rate_cnt.get_text()
             rate_cnt = rate_cnt[1:-1]
         else:
-            rate_cnt = '평점 수 없음'
-            # print('---<평점 수 없는 상품 제외>---')
             continue
 
         link = item.find('a', attrs={'class': 'search-product-link'})['href']
 
         if int(rate_cnt) >= 100 and float(rate) >= 5.0:
-            # print(name, price, rate, rate_cnt)
             print(f'제품명: {name}')
             print(f'가격: {price}')
             print(f'평점(리뷰): {rate}({rate_cnt})')
